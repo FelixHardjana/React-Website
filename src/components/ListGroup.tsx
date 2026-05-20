4cc3bc29d1fec90e5f1fc03cffafa86df13f3d9f
Felix Haardjana <fhardjana@gmail.com>
2026-05-20 16:17:57 -0400

    learning props reusability

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,26 +1,24 @@
 import { useState } from "react";
 
-function ListGroup() {
-  let items = [
-    'New York',
-    'San Francisco',
-    'Tokyo',
-    'London',
-    'Paris'
-  ]
+  // { items: [], heading: string}
+  interface Props{
+    items: string[];
+    heading: string;
+  }
 
+
+
+function ListGroup(props: Props) {
   // Hook
   const [selectedIndex, setSelectedIndex] = useState(-1);
   
-
-
   return(
   <>
   <h1>List</h1>
   {/* { items.length === 0 ? <p> No item found</p> : null } */}
-  { items.length === 0 && <p>No item found</p>}
+  { props.items.length === 0 && <p>No item found</p>}
   <ul className="list-group">
-      {items.map((item, index) => (
+      {props.items.map((item, index) => (
         <li className={ selectedIndex === index ? 'list-group-item active' : 'list-group-item' } onClick={() => {setSelectedIndex(index);}} key={item}>{item}</li>
       ))}
   </ul>
